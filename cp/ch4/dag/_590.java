d2d06f26207600457d581c6805051cb4046afb6d
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2016-10-21 23:00:06 +0200

    Msg

--- cp/ch4/dag/_590.java
+++ cp/ch4/dag/_590.java
@@ -10,7 +10,8 @@ Author: yemelyanov
 public class _590 {
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
-    
+    while(s.hasNext()){
+      System.out.println(s.next());
+    }
   }
 }
-
