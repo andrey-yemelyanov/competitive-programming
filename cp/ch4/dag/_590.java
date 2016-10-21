d6cb566220b7fb250604dce49e6fce7148fc6ea6
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2016-10-21 22:55:12 +0200

    UVA 590 Always on the run

---
+++ cp/ch4/dag/_590.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 590 Always on the run
+Problem url: https://uva.onlinejudge.org/external/5/590.pdf
+Author: yemelyanov
+*/
+public class _590 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    
+  }
+}
+
