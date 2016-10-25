566e36a5722240e905a6d41ea71435286d0b480b
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2016-10-25 21:54:58 +0200

    sync

---
+++ cp-book/ch4/dag/_10913.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 10913 Walking on a Grid
+Problem url: https://uva.onlinejudge.org/external/109/10913.pdf
+Author: Andrey Yemelyanov
+*/
+public class _10913 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+
+  }
+}
