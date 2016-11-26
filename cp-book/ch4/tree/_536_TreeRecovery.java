e540042a6dee160afd89ff21ab38ff8061a783da
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2016-11-26 09:04:06 +0100

    UVA 536 Tree Recovery

---
+++ cp-book/ch4/tree/_536_TreeRecovery.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 536 Tree Recovery
+Problem url: https://uva.onlinejudge.org/external/5/536.pdf
+Author: Andrey Yemelyanov
+*/
+public class _536_TreeRecovery {
+  static class Node{
+    public String value;
+    public Node left;
+    public Node right;
+    public Node(String value){
+      this.value = value;
+    }
+  }
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    while(s.hasNext()){
+      String preorder = s.next(); String inorder = s.next();
+      StringBuilder sb = new StringBuilder();
+      postorder(buildTree(preorder, inorder), sb);
+      System.out.println(sb.toString());
+    }
+  }
+
+  static void postorder(Node root, StringBuilder traversal){
+    if(root == null) return;
+    postorder(root.left, traversal);
+    postorder(root.right, traversal);
+    traversal.append(root.value);
+  }
+
+  static Node buildTree(String preorder, String inorder){
+    if(preorder.length() == 1) return new Node(preorder);
+    String root = preorder.substring(0, 1);
+    Node rootNode = new Node(root);
+    String leftInorder = inorder.substring(0, inorder.indexOf(root));
+    String rightInorder = inorder.substring(inorder.indexOf(root) + 1);
+    if(!leftInorder.equals("")){
+      String leftPreorder = preorder.substring(1, leftInorder.length() + 1);
+      rootNode.left = buildTree(leftPreorder, leftInorder);
+    }
+    if(!rightInorder.equals("")){
+      String rightPreorder = preorder.substring(leftInorder.length() + 1);
+      rootNode.right = buildTree(rightPreorder, rightInorder);
+    }
+    return rootNode;
+  }
+}
