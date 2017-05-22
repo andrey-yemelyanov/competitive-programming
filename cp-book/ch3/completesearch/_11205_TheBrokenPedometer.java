7fc7ee8435b0ed6bf06075d5b2ba1d10ffaa468c
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2017-05-22 14:42:13 +0200

    UVA 11205 The broken pedometer

--- cp-book/ch3/completesearch/_11205_TheBrokenPedometer.java
+++ cp-book/ch3/completesearch/_11205_TheBrokenPedometer.java
@@ -1,41 +1,8 @@
-package helvidios.cp.ch3.completesearch;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class _11205_TheBrokenPedometer {
 	public static void main(String... args){
-		String data = "2\r\n" + 
-				"7\r\n" + 
-				"10\r\n" + 
-				"1 1 1 0 1 1 1\r\n" + 
-				"0 0 1 0 0 1 0\r\n" + 
-				"1 0 1 1 1 0 1\r\n" + 
-				"1 0 1 1 0 1 1\r\n" + 
-				"0 1 1 1 0 1 0\r\n" + 
-				"1 1 0 1 0 1 1\r\n" + 
-				"1 1 0 1 1 1 1\r\n" + 
-				"1 0 1 0 0 1 0\r\n" + 
-				"1 1 1 1 1 1 1\r\n" + 
-				"1 1 1 1 0 1 1\r\n" + 
-				"6\r\n" + 
-				"10\r\n" + 
-				"0 1 1 1 0 0\r\n" + 
-				"1 0 0 0 0 0\r\n" + 
-				"1 0 1 0 0 0\r\n" + 
-				"1 1 0 0 0 0\r\n" + 
-				"1 1 0 1 0 0\r\n" + 
-				"1 0 0 1 0 0\r\n" + 
-				"1 1 1 0 0 0\r\n" + 
-				"1 1 1 1 0 0\r\n" + 
-				"1 0 1 1 0 0\r\n" + 
-				"0 1 1 0 0 0";
-		Scanner scanner = new Scanner(data);
+		Scanner scanner = new Scanner(System.in);
 		int nTestCases = scanner.nextInt();
 		while(nTestCases-- > 0){
 			int p = scanner.nextInt();
@@ -48,47 +15,31 @@ public class _11205_TheBrokenPedometer {
 				}
 				codes[i] = Integer.parseInt(line, 2);
 			}
-			System.out.println(solve(p, codes));
+			System.out.println(getMinActiveLeds(p, codes));
 		}
 		scanner.close();
 	}
-	
-	/**
-	 * Generates bitmasks consisting of max p bits and returns a map that associates parity value from 1 to p
-	 * with a list of corresponding masks.
-	 */
-	public static Map<Integer, List<Integer>> generateBitmasks(int p){
-		Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
-		for(int i = 1; i <= Math.pow(2, p) - 1; i++){
-			int bitParity = bitParity(i);
-			if(!map.containsKey(bitParity)){
-				map.put(bitParity, new ArrayList<Integer>());
+
+	static int getMinActiveLeds(int p, int[] symbols){
+		int minLeds = p + 1;
+		for(int activeLeds = 0; activeLeds < (2 << p); activeLeds++){
+			Set<Integer> s = new HashSet<>();
+			for(int symbol : symbols){
+				s.add(symbol & activeLeds);
 			}
-			map.get(bitParity).add(i);
-		}
-		return map;
-	}
-	
-	static int bitParity(int mask){
-		int parity = 0;
-		while(mask != 0){
-			parity++;
-			mask ^= (mask & ~(mask - 1));
-		}
-		return parity;
-	}
-	
-	public static int solve(int p, int[] codes){
-		Map<Integer, List<Integer>> masks = generateBitmasks(p);
-		for(int i = 1; i <= p; i++){
-			for(int mask : masks.get(i)){
-				Set<Integer> s = new HashSet<Integer>();
-				for(int j = 0; j < codes.length; j++){
-					s.add(codes[j] & mask);
-				}
-				if(s.size() == codes.length) return i;
+			if(s.size() == symbols.length) {
+				minLeds = Math.min(minLeds, countSetBits(activeLeds));
 			}
 		}
-		return p;
+		return minLeds;
+	}
+
+	static int countSetBits(int n){
+		int count = 0;
+		while(n != 0){
+			if((n & 1) != 0) count++;
+			n = n >> 1;
+		}
+		return count;
 	}
 }
