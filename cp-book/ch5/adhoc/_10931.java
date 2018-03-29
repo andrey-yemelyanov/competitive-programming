cdf1e6242a3c8d154f25b1ebbc890f53745da02b
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2018-03-29 17:35:09 +0200

    10931 Parity

---
+++ cp-book/ch5/adhoc/_10931.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 10931 Parity
+Problem url: https://uva.onlinejudge.org/external/109/10931.pdf
+Author: Andrey Yemelyanov
+*/
+public class _10931 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    while(s.hasNext()){
+      int n = s.nextInt();
+      if(n == 0) break;
+      System.out.printf("The parity of %s is %d (mod 2).\n", bitString(n), parity(n));
+    }
+  }
+
+  static String bitString(int n){
+    String bitString = "";
+    while(n > 0){
+      bitString = (n & 1) + bitString;
+      n >>= 1;
+    }
+    return bitString;
+  }
+
+  static int parity(int n){
+    int parity = 0;
+    while(n > 0){
+      if((n & 1) == 1) parity++;
+      n >>= 1;
+    }
+    return parity;
+  }
+}
