5cf12208a721ef25087487bb9b0f290e6bba8f88
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2018-03-31 11:21:58 +0200

    10235 Simply Emirp

---
+++ cp-book/ch5/bigint/_10235.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import static java.lang.Math.*;
+import static java.lang.System.*;
+import java.util.stream.*;
+import java.math.BigInteger;
+
+/*
+Problem name: 10235 Simply Emirp
+Problem url: https://uva.onlinejudge.org/external/102/10235.pdf
+Author: Andrey Yemelyanov
+*/
+public class _10235 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(in);
+    while(s.hasNext()){
+      int n = s.nextInt();
+      if(!isPrime(n)) System.out.printf("%d is not prime.\n", n);
+      else if(isEmirp(n)) System.out.printf("%d is emirp.\n", n);
+      else System.out.printf("%d is prime.\n", n);
+    }
+  }
+
+  static boolean isEmirp(int n){
+    int reversed = reverseDigits(n);
+    if(reversed == n) return false;
+    return isPrime(reverseDigits(n));
+  }
+
+  static boolean isPrime(int n){
+    return BigInteger.valueOf(n).isProbablePrime(10);
+  }
+
+  static int reverseDigits(int n){
+    int reversed = 0;
+    while(n != 0){
+      int digit = n % 10;
+      reversed = reversed * 10 + digit;
+      n /= 10;
+    }
+    return reversed;
+  }
+}
