2b74684899403df23ec035c2cc1095bdc3c21e52
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2018-06-11 17:51:31 +0200

    11048 work in progress...

---
+++ cp-book/ch6/adhoc/_11048.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 11048 Automatic Correction of Misspellings
+Problem url: https://uva.onlinejudge.org/external/110/11048.pdf
+Author: Andrey Yemelyanov
+*/
+public class _11048 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+
+  }
+
+  static String getMisspelling(String str, Map<String, Integer> dict, ){
+
+  }
+
+  static List<String> oneLetterMissing(String str){
+
+  }
+
+  static List<String> oneLetterTooMuch(String str){
+
+  }
+
+  static List<String> oneLetterWrong(String str){
+
+  }
+
+  static List<String> twoAdjacentLettersWrong(String str){
+
+  }
+}
