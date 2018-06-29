ad617a7a914b4b09895545c4c7949cbaaaf1d595
Andrey Yemelyanov <andrey.yemelyanov@gmail.com>
2018-06-29 13:58:07 +0200

    11576 Scrolling sign

---
+++ cp-book/ch6/stringmatching/_11576.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import static java.lang.Math.*;
+import java.util.stream.*;
+
+/*
+Problem name: 11576 Scrolling Sign
+Problem url: https://uva.onlinejudge.org/external/115/11576.pdf
+Author: Andrey Yemelyanov
+*/
+public class _11576 {
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int nTests = s.nextInt();
+    while(nTests-- > 0){
+      s.nextInt(); int n = s.nextInt();
+      List<String> words = new ArrayList<>();
+      while(n-- > 0) words.add(s.next());
+      System.out.println(countLetters(words));
+    }
+  }
+
+  static int countLetters(List<String> words){
+    Collections.reverse(words);
+    int wordLen = words.get(0).length();
+    int nLetters = wordLen;
+    for(int i = 1; i < words.size(); i++){
+      int[] b = kmpPreprocess(words.get(i - 1) + words.get(i));
+      int n = b[b.length - 1];
+      n = min(wordLen, n);
+      nLetters += wordLen - n;
+    }
+    return nLetters;
+  }
+
+  static int[] kmpPreprocess(String pattern){
+    int[] b = new int[pattern.length() + 1];
+    int i = 0; int j = -1; b[0] = -1;
+    while(i < pattern.length()){
+      while(j >= 0 && pattern.charAt(i) != pattern.charAt(j)) j = b[j];
+      i++; j++;
+      b[i] = j;
+    }
+    return b;
+  }
+}
